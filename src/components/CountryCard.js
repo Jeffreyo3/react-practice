6f3b68e2257c22d91dd63684d7b2289b68b79412
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-05-14 16:30:45 -0700

    Convert img style to styled-components

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -7,12 +7,17 @@ border: 1px solid slategrey;
 border-radius: 20px;
 `
 
+const Img = styled.img`
+width: 250px;
+height: 150px;
+`
+
 export default function CountryCard({ country }) {
 
     return (
         <Card>
             <h3>{country.name}</h3>
-            <img src={`${country.flag}`} style={{ width: '250px', height: '150px' }} alt={`Flag of ${country.name}`} />
+            <Img src={`${country.flag}`} alt={`Flag of ${country.name}`} />
             <p>Capital: {country.capital}</p>
             <p>Population: {country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
         </Card>
